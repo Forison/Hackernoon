6bf91b402ec34470ec37989bb2ec208b357353eb
forison <addoboakyeforison@gmail.com>
2021-03-10 15:32:03 -0500

    adjust code-container styling

--- src/components/Navbar/Navbar4.jsx
+++ src/components/Navbar/Navbar4.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Navbar4 = () => {
   return (
-    <nav className="navbar fifteen-px">
-    Step by Step Guide to Create 3 Different Types of Loading Screens in React by @CodeBucks
+    <nav className="fifteen-px">
+    Step by Step Guide to Create 3 
+    Different Types of Loading Screens in React by <strong className="noon-border">@CodeBucks</strong>.
   </nav>
   )
 }
